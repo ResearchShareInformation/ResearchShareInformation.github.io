c1373d0678471cc252774b56860e648876072c8c
ResearchShareInformation <justmathtopass@gmail.com>
2025-03-15 10:49:43 -0400

    Add files via upload

--- drone.js
+++ drone.js
@@ -2,11 +2,11 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     // Example of dynamically loading slides or content
-    const slides = [
+    /*const slides = [
         "Slide 1: FPV Drone Basics",
         "Slide 2: FPV Technology Overview",
         "Slide 3: Advanced FPV Components"
-    ];
+    ];*/
     
     const slideContainer = document.createElement('div');
     slideContainer.id = 'slide-container';
